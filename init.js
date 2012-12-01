1ede23d62c00a287bb2e042a935e365c8617bf15
Tran Xuan Truong <tmtxt@Trans-MacBook-Pro.local>
2012-12-01 20:04:48 +0700

    fix some web jumps

--- init.js
+++ init.js
@@ -216,18 +216,13 @@ define_webjump("js", "https://developer.mozilla.org/en-US/search?q=%s",
 define_webjump("dantri", "http://dantri.com.vn");
 define_webjump("vnexpress", "http://vnexpress.net");
 define_webjump("ti", "http://tinhte.vn");
-define_webjump("tinhte", "http://tinhte.vn");
 define_webjump("macvn", "http://macvn.com");
 define_webjump("sohoa", "http://sohoa.net");
 define_webjump("voz", "http://voz.vn");
-define_webjump("gsm", "http://gsm.vn");
-define_webjump("handheld", "http://handheld.com.vn");
 define_webjump("pcworldVN", "http://pcworld.com.vn");
 define_webjump("hdvietnam", "http://hdvietnam.com");
 define_webjump("yeuamnhac", "http://yeuamnhac.com");
 define_webjump("demonoid", "http://demonoid.me");
-define_webjump("thecentralbox", "http://thecentralbox.com");
-define_webjump("facebook", "http://www.facebook.com");
 define_webjump("f", "http://www.facebook.com");
 define_webjump("lqd-dd", "http://lqd-dd.com/");
 define_webjump("googlevn", "http://google.com.vn/");
@@ -236,9 +231,9 @@ define_webjump("googlevn", "http://google.com.vn/");
 interactive("open-facebook-new", "Open Facebook New Buffer", "follow-new-buffer",
             $browser_object = "http://facebook.com/");
 define_key(content_buffer_normal_keymap, "C-1", "open-facebook-new");
-interactive("open-lqddd-new", "Open Lqd-dd New Buffer", "follow-new-buffer",
-            $browser_object = "http://lqd-dd.com/");
-define_key(content_buffer_normal_keymap, "C-2", "open-lqddd-new");
+interactive("open-tinhte-new", "Open Tinhte New Buffer", "follow-new-buffer",
+            $browser_object = "http://tinhte.vn/");
+define_key(content_buffer_normal_keymap, "C-2", "open-tinhte-new");
 
 // Use history not bookmark?
 url_completion_use_history = true;
