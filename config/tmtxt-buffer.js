c88569f7efb63d7daccd99dbb537579ede0272ae
tmtxt <tmtxt@txt-mac.local>
2013-09-14 22:46:04 +0700

    new file for buffer configuration

---
+++ config/tmtxt-buffer.js
@@ -0,0 +1,34 @@
+//this is for replacing the built in kill-this-buffer command
+//the command tmtxt-close-and-save-current-buffer will save the URL of the
+//current buffer beforing closing it
+//later when you want to reopen it, just call the command tmtxt-open-closed-buffer
+//first, create an array to hold the closed buffers
+var tmtxt_closed_buffers = new Array();
+//save the URL of the current buffer before closing it
+interactive("tmtxt-close-and-save-current-buffer", "close and save the current buffer for later restore",
+			function(I) {
+			  if(tmtxt_closed_buffers.length==10){
+				tmtxt_closed_buffers.shift(); // remove older item to save
+				// memory, just save maximum 10 buffers
+			  }
+			  tmtxt_closed_buffers.push(I.buffer.document.URL); //add one item
+			  kill_buffer(I.buffer); //kill the current buffer
+			});
+//reopen the last closed buffer and remove it from the array
+interactive("tmtxt-open-closed-buffer", "open the last closed buffer", 
+			function(I){
+			  if(tmtxt_closed_buffers.length>0){
+				load_url_in_new_buffer(tmtxt_closed_buffers[tmtxt_closed_buffers.length-1],I.window);
+				tmtxt_closed_buffers.pop();
+			  }
+			});
+
+//// kill current buffer
+define_key(default_global_keymap, "w", "tmtxt-close-and-save-current-buffer");
+define_key(default_global_keymap, "A-w", "tmtxt-close-and-save-current-buffer");
+define_key(default_global_keymap, "A-k", "tmtxt-close-and-save-current-buffer");
+//reopen last closed buffer
+define_key(default_global_keymap, "A-W", "tmtxt-open-closed-buffer")
+define_key(default_global_keymap, "A-T", "tmtxt-open-closed-buffer")
+
+provide("tmtxt-buffer");
--- init.js
+++ init.js
@@ -43,34 +43,12 @@ add_hook("mode_line_hook", mode_line_adder(loading_count_widget), true);
 tmtxt_add_path("config");
 require("tmtxt-appearance.js");
 require("tmtxt-webjumps.js");
+require("tmtxt-buffer.js");
 
 // Auto load the auto-save session when conkeror starts
 session_auto_save_auto_load = true;
 
-//this is for replacing the built in kill-this-buffer command
-//the command tmtxt-close-and-save-current-buffer will save the URL of the
-//current buffer beforing closing it
-//later when you want to reopen it, just call the command tmtxt-open-closed-buffer
-//first, create an array to hold the closed buffers
-var tmtxt_closed_buffers = new Array();
-//save the URL of the current buffer before closing it
-interactive("tmtxt-close-and-save-current-buffer", "close and save the current buffer for later restore",
-			function(I) {
-			  if(tmtxt_closed_buffers.length==10){
-				tmtxt_closed_buffers.shift(); // remove older item to save
-				// memory, just save maximum 10 buffers
-			  }
-			  tmtxt_closed_buffers.push(I.buffer.document.URL); //add one item
-			  kill_buffer(I.buffer); //kill the current buffer
-			});
-//reopen the last closed buffer and remove it from the array
-interactive("tmtxt-open-closed-buffer", "open the last closed buffer", 
-			function(I){
-			  if(tmtxt_closed_buffers.length>0){
-				load_url_in_new_buffer(tmtxt_closed_buffers[tmtxt_closed_buffers.length-1],I.window);
-				tmtxt_closed_buffers.pop();
-			  }
-			});
+
 
 define_key(default_global_keymap, "A-i", "inspect-chrome");
 define_key(read_buffer_keymap, "A-i", "inspect-chrome");
@@ -172,12 +150,6 @@ define_key(content_buffer_normal_keymap, "A-f", "follow-new-buffer-background");
 undefine_key(default_global_keymap, "q");
 define_key(content_buffer_normal_keymap, "q", "follow-new-buffer-background");
 define_key(default_global_keymap, "C-t", "find-url-new-buffer");
-//// kill current buffer
-define_key(default_global_keymap, "w", "tmtxt-close-and-save-current-buffer");
-define_key(default_global_keymap, "A-w", "tmtxt-close-and-save-current-buffer");
-define_key(default_global_keymap, "A-k", "tmtxt-close-and-save-current-buffer");
-//reopen last closed buffer
-define_key(default_global_keymap, "A-W", "tmtxt-open-closed-buffer")
 //// word selection
 define_key(content_buffer_normal_keymap, "S-M-right", "cmd_selectWordNext");
 define_key(content_buffer_normal_keymap, "S-M-left", "cmd_selectWordPrevious");
@@ -378,7 +350,6 @@ interactive("readability_arc90",
                 _readability_print_css.type = 'text/css'
                 document.getElementsByTagName('head')[0].appendChild(_readability_print_css)
             });
-define_key(content_buffer_normal_keymap, "j", "readability_arc90");
 
 // What's this?
 function repl_context() {
