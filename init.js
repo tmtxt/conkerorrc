ddef575388adca341f5b435e0632c54f529efc57
tmtxt <tmtxt@txt-mac.local>
2013-09-14 23:33:34 +0700

    separate file for readability config

--- init.js
+++ init.js
@@ -39,6 +39,7 @@ require("tmtxt-webjumps.js");
 require("tmtxt-buffer.js");
 require("tmtxt-modeline.js");
 require("tmtxt-keybindings.js");
+require("tmtxt-readability.js");
 
 // Auto load the auto-save session when conkeror starts
 session_auto_save_auto_load = true;
@@ -71,80 +72,7 @@ download_buffer_automatic_open_target = OPEN_NEW_BUFFER_BACKGROUND;
 
 clicks_in_new_buffer_target = OPEN_NEW_BUFFER_BACKGROUND;
 
-// Replacement of built-in C-x b
-// minibuffer.prototype.read_recent_buffer = function () {
-//     var window = this.window;
-//     var buffer = this.window.buffers.current;
-//     keywords(arguments, $prompt = "Buffer:",
-//              $default = buffer,
-//              $history = "buffer");
-//     var buffers = window.buffers.buffer_list.slice(0);
-//     buffers.push(buffers.shift());
-//     var completer = all_word_completer(
-//         $completions = buffers,
-//         $get_string = function (x) x.title,
-//         $get_description = function (x) x.description);
-//     var result = yield this.read(
-//         $keymap = read_buffer_keymap,
-//         $prompt = arguments.$prompt,
-//         $history = arguments.$history,
-//         $completer = completer,
-//         $match_required = true,
-//         $auto_complete = "buffer",
-//         $auto_complete_initial = true,
-//         $auto_complete_delay = 0,
-//         $default_completion = arguments.$default);
-//     yield co_return(result);
-// };
 
-
-
-// Readability tool
-interactive("readability_arc90",
-            "Readability is a simple tool that makes reading on the web more enjoyable by removing the clutter around what you are reading",
-            function readability_arc90(I) {
-                var document = I.window.buffers.current.document;
-
-                var readConvertLinksToFootnotes = false;
-                var readStyle = 'style-newspaper';
-                var readSize = 'size-medium';
-                var readMargin = 'margin-wide';
-
-                var _readability_readStyle = document.createElement('SCRIPT');
-                _readability_readStyle.text = 'var readStyle = \'' + readStyle + '\';';
-                document.getElementsByTagName('head')[0].appendChild(_readability_readStyle);
-
-                var _readability_readSize = document.createElement('SCRIPT');
-                _readability_readSize.text = 'var readSize = \'' + readSize + '\';';
-                document.getElementsByTagName('head')[0].appendChild(_readability_readSize);
-
-                var _readability_readMargin = document.createElement('SCRIPT');
-                _readability_readMargin.text = 'var readMargin = \'' + readMargin + '\';';
-                document.getElementsByTagName('head')[0].appendChild(_readability_readMargin);
-
-                var _readability_readConvertLinksToFootnotes = document.createElement('SCRIPT');
-                _readability_readConvertLinksToFootnotes.text = 'var readConvertLinksToFootnotes = ' + readConvertLinksToFootnotes + ';';
-                document.getElementsByTagName('head')[0].appendChild(_readability_readConvertLinksToFootnotes);
-
-                var _readability_script = document.createElement('script')
-                _readability_script.type='text/javascript'
-                _readability_script.src='http://lab.arc90.com/experiments/readability/js/readability.js?x='+(Math.random())
-                document.documentElement.appendChild(_readability_script)
-
-                var _readability_css = document.createElement('link')
-                _readability_css.rel = 'stylesheet'
-                _readability_css.href = 'http://lab.arc90.com/experiments/readability/css/readability.css'
-                _readability_css.type = 'text/css'
-                _readability_css.media = 'all'
-                document.documentElement.appendChild(_readability_css)
-
-                var _readability_print_css = document.createElement('link')
-                _readability_print_css.rel = 'stylesheet'
-                _readability_print_css.href = 'http://lab.arc90.com/experiments/readability/css/readability-print.css'
-                _readability_print_css.media = 'print'
-                _readability_print_css.type = 'text/css'
-                document.getElementsByTagName('head')[0].appendChild(_readability_print_css)
-            });
 
 // What's this?
 function repl_context() {
