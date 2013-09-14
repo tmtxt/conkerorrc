acd502010361c771efb4d1843293793bed549dc6
tmtxt <tmtxt@txt-mac.local>
2013-09-14 22:19:19 +0700

    move colors-toggle into appearance.js

--- config/appearance.js
+++ config/appearance.js
@@ -32,4 +32,30 @@ let(st_on = false) {
   };
 };
 
+//colors-toggle
+interactive("colors-toggle", "toggle between document and forced colors",
+            function (I) {
+                var p = "browser.display.use_document_colors";
+                if (get_pref(p)) {
+                    session_pref("browser.active_color", "yellow");
+                    session_pref("browser.anchor_color", "#4986dd");
+                    session_pref("browser.display.background_color", "#0C141E");
+                    session_pref("browser.display.foreground_color", "#A0AFA8");
+                    session_pref("browser.display.focus_background_color", "green"); // ?
+                    session_pref("browser.display.focus_text_color", "red"); // ?
+                    session_pref("browser.visited_color", "#805DBB");
+                    session_pref(p, false);
+                } else {
+                    session_pref("browser.active_color", "#EE0000");
+                    session_pref("browser.anchor_color", "#0000EE");
+                    session_pref("browser.display.background_color", "#FFFFFF");
+                    session_pref("browser.display.foreground_color", "#000000");
+                    session_pref("browser.display.focus_background_color", "#117722");
+                    session_pref("browser.display.focus_text_color", "#FFFFFF");
+                    session_pref("browser.visited_color", "#551A8B");
+                    session_pref(p, true);
+                }
+            });
+define_key(default_global_keymap, "A-n", "colors-toggle");
+
 provide("appearance");
--- init.js
+++ init.js
@@ -16,9 +16,6 @@ modifiers.A = new modifier(function (event) { return event.metaKey; },
 
 define_key(default_global_keymap, "A-`", null, $fallthrough);
 
-// install extension from unknow source
-session_pref("xpinstall.whitelist.required", false);
-
 // add directory dir inside .conkerorrc to load_paths
 function tmtxt_add_path(dir) {
     let (path = get_home_directory()) {
@@ -458,97 +455,6 @@ function repl_context() {
     return ctx;
 }
 
-
-// Darken page - for reading webpage in the night
-// TODO: use a boolean to keep track of dark/light state
-// TODO: add hook for new buffers
-function foo (I) {
-    var styles='* { background: black !important; color: gray !important; }'+
-        ':link, :link * { color: #4986dd !important; }'+
-        ':visited, :visited * { color: #d75047 !important; }';
-    var wd = conkeror.get_recent_conkeror_window();
-    // FIXME: better way for this?
-    if (typeof foo.darkened == 'undefined') {
-        foo.darkened = true;
-    } else {
-        foo.darkened = !foo.darkened;
-    }
-    // FIXME: content buffers only
-    for (var i=0; i < wd.buffers.count; i++) {
-        var document = I.window.buffers.get_buffer(i).document;
-        var id = 'ubolonton/darken_page';
-        let (newSS = document.getElementById(id)) {
-            if (newSS == null) {
-                if (foo.darkened) {
-                    newSS = document.createElement('link');
-                    newSS.rel = 'stylesheet';
-                    newSS.href = 'data:text/css,' + escape(styles);
-                    newSS.id = id;
-                    document.getElementsByTagName("head")[0].appendChild(newSS);
-                }
-            } else {
-                if (!foo.darkened) {
-                    newSS.parentNode.removeChild(newSS);
-                }
-            }
-        }
-    }
-}
-interactive("foo", "Toggle darkening all pages", foo);
-define_key(content_buffer_normal_keymap, "A-D", "foo");
-function toggle_darkened_page (I) {
-    var styles='* { background: black !important; color: gray !important; }'+
-        ':link, :link * { color: #4986dd !important; }'+
-        ':visited, :visited * { color: #d75047 !important; }';
-    var document = I.buffer.document;
-    var id = 'ubolonton/darken_page';
-    let (newSS = document.getElementById(id)) {
-        if (newSS == null) {
-            newSS = document.createElement('link');
-            newSS.rel = 'stylesheet';
-            newSS.href = 'data:text/css,' + escape(styles);
-            newSS.id = id;
-            document.getElementsByTagName("head")[0].appendChild(newSS);
-        } else {
-            newSS.parentNode.removeChild(newSS);
-        }
-    }
-}
-interactive("toggle-darkened-page", "Toggle darkening the page in an attempt to save your eyes.",
-            toggle_darkened_page);
-define_key(content_buffer_normal_keymap, "A-d", "toggle-darkened-page");
-
-// FIXME: the page is darkened only after the DOM is finished
-// loading. This causes flickering, which is sub-optimal
-
-define_variable("darkened", false, "Darkened or not");
-
-function set_darkness (buffer) {
-    var styles='* { background: black !important; color: gray !important; }'+
-        ':link, :link * { color: #4986dd !important; }'+
-        ':visited, :visited * { color: #d75047 !important; }';
-    var document = buffer.document;
-    var id = 'ubolonton/darken_page';
-    let (newSS = document.getElementById(id)) {
-        if (newSS == null) {
-            if (darkened) {
-                newSS = document.createElement('link');
-                newSS.rel = 'stylesheet';
-                newSS.href = 'data:text/css,' + escape(styles);
-                newSS.id = id;
-                document.getElementsByTagName("head")[0].appendChild(newSS);
-            }
-        } else {
-            if (!darkened) {
-                newSS.parentNode.removeChild(newSS);
-            }
-        }
-    }
-}
-
-add_hook("buffer_dom_content_loaded_hook", set_darkness, false, true);
-add_hook("buffer_loaded_hook", set_darkness, false, true);
-
 // Allow installing extension from any source
 session_pref("xpinstall.whitelist.required", false);
 
@@ -587,33 +493,6 @@ interactive("viewmarks",
     function (I) {
         make_chrome_window('chrome://viewmarks/content/viewmark.xul');
     });
-
-//colors-toggle
-//replace darken-page mode
-interactive("colors-toggle", "toggle between document and forced colors",
-            function (I) {
-                var p = "browser.display.use_document_colors";
-                if (get_pref(p)) {
-                    session_pref("browser.active_color", "yellow");
-                    session_pref("browser.anchor_color", "#4986dd");
-                    session_pref("browser.display.background_color", "#0C141E");
-                    session_pref("browser.display.foreground_color", "#A0AFA8");
-                    session_pref("browser.display.focus_background_color", "green"); // ?
-                    session_pref("browser.display.focus_text_color", "red"); // ?
-                    session_pref("browser.visited_color", "#805DBB");
-                    session_pref(p, false);
-                } else {
-                    session_pref("browser.active_color", "#EE0000");
-                    session_pref("browser.anchor_color", "#0000EE");
-                    session_pref("browser.display.background_color", "#FFFFFF");
-                    session_pref("browser.display.foreground_color", "#000000");
-                    session_pref("browser.display.focus_background_color", "#117722");
-                    session_pref("browser.display.focus_text_color", "#FFFFFF");
-                    session_pref("browser.visited_color", "#551A8B");
-                    session_pref(p, true);
-                }
-            });
-define_key(default_global_keymap, "A-n", "colors-toggle");
 
 // get tiny url for the current page
 // press * q and then c to generate and copy the tinyurl into clipboard
