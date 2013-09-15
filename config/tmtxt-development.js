90482533b55167f0917c77556cb19897809abf1f
tmtxt <tmtxt@txt-mac.local>
2013-09-15 14:19:37 +0700

    add firebug command

--- config/tmtxt-development.js
+++ config/tmtxt-development.js
@@ -7,4 +7,18 @@ let (mozrepl_init = get_home_directory()) {
   session_pref('extensions.mozrepl.initUrl', make_uri(mozrepl_init).spec);
 }
 
+// firebug
+define_variable("firebug_url",
+    "http://getfirebug.com/releases/lite/1.2/firebug-lite-compressed.js");
+
+function firebug (I) {
+    var doc = I.buffer.document;
+    var script = doc.createElement('script');
+    script.setAttribute('type', 'text/javascript');
+    script.setAttribute('src', firebug_url);
+    script.setAttribute('onload', 'firebug.init();');
+    doc.body.appendChild(script);
+}
+interactive("firebug", "open firebug lite", firebug);
+
 provide("tmtxt-development");
