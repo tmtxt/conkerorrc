1049b1cc4b7cf02fbaf8d6294b484752e2e6d918
tmtxt <tmtxt@txt-mac.local>
2013-09-15 12:01:02 +0700

    move mozrepl into modules

--- init.js
+++ init.js
@@ -2,6 +2,7 @@
 user_pref('extensions.mozrepl.autoStart', true);
 let (mozrepl_init = get_home_directory()) {
   mozrepl_init.appendRelativePath(".conkerorrc");
+  mozrepl_init.appendRelativePath("modules");
   mozrepl_init.appendRelativePath(".mozrepl-conkeror.js");
   session_pref('extensions.mozrepl.initUrl', make_uri(mozrepl_init).spec);
 }
