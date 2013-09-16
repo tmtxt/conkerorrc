91662e22b2e3fbbcc862ae4dc463380aff8bbe50
tmtxt <tmtxt@txt-mac.local>
2013-09-17 00:10:52 +0700

    modify tmtxt-facebook to point the submodule

--- config/tmtxt-facebook.js
+++ config/tmtxt-facebook.js
@@ -1,56 +1,17 @@
-// facebook mode
+// extended facebook mode
+let (path = get_home_directory()) {
+  // add to load path
+  path.appendRelativePath(".conkerorrc");
+  path.appendRelativePath("modules");
+  path.appendRelativePath("cefm");
+  load_paths.unshift(make_uri(path).spec);
 
-require("content-buffer.js");
-
-define_keymap("facebook_keymap", $display_name = "facebook");
-
-// facebook shortcut keys
-define_key(facebook_keymap, "j", null, $fallthrough);
-define_key(facebook_keymap, "k", null, $fallthrough);
-define_key(facebook_keymap, "return", null, $fallthrough);
-define_key(facebook_keymap, "q", null, $fallthrough);
-define_key(facebook_keymap, "/", null, $fallthrough);
-define_key(facebook_keymap, "l", null, $fallthrough);
-define_key(facebook_keymap, "m", null, $fallthrough);
-define_key(facebook_keymap, "c", null, $fallthrough);
-
-// function for inspecting and finding the link of selected story
-function facebook_mode_find_story_link(I, open_url_func){
-  var doc = I.buffer.document;
-  var selectedStory = doc.querySelector(".selectedStorySimple");
-  if(selectedStory == null){
-	I.minibuffer.message("No selected story");
-  } else {
-	var timestamp = selectedStory.querySelector(".timestamp");
-	if(timestamp == null){
-	  I.minibuffer.message("Cannot find timestamp link");
-	} else {
-	  var link = timestamp.parentNode;
-	  open_url_func(link, i.window);
-	}
-  }
-}
-
-// interactive command to open selected story
-// new buffer
-interactive("facebook-open-current-story-new-buffer", null, function (I) {
-  facebook_mode_find_story_link(I, load_url_in_new_buffer);
-  });
-// new buffer background
-interactive("facebook-open-current-story-new-buffer-background", null, function (I) {
-  facebook_mode_find_story_link(I, load_url_in_new_buffer_background);
-  });
+  // include the library
+  require("conkeror-extended-facebook-mode.js");  
+};
 
 // bind key
 define_key(facebook_keymap, "C-M-o", "facebook-open-current-story-new-buffer");
 define_key(facebook_keymap, "C-O", "facebook-open-current-story-new-buffer-background");
-
-define_keymaps_page_mode("facebook-mode",
-    build_url_regexp($domain = "facebook",
-                     $allow_www = true),
-    { normal: facebook_keymap },
-    $display_name = "Facebook");
-
-page_mode_activate(facebook_mode);
 
 provide("tmtxt-facebook");
