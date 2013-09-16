6a4d00b1cfef4a1a6af65053f45169a49aa9bcf5
tmtxt <tmtxt@txt-mac.local>
2013-09-16 23:10:40 +0700

    pass function to open url to inspecting function

--- config/tmtxt-facebook.js
+++ config/tmtxt-facebook.js
@@ -14,22 +14,8 @@ define_key(facebook_keymap, "l", null, $fallthrough);
 define_key(facebook_keymap, "m", null, $fallthrough);
 define_key(facebook_keymap, "c", null, $fallthrough);
 
-// interactive command to open selected story
-// new buffer
-interactive("facebook-open-current-story-new-buffer", null, function (I) {
-  var link = facebook_mode_find_story_link(I);
-  load_url_in_new_buffer(link,I.window);
-  });
-// new buffer background
-interactive("facebook-open-current-story-new-buffer-background", null, function (I) {
-  var link = facebook_mode_find_story_link(I);
-  load_url_in_new_buffer_background(link,I.window);
-  });
-define_key(facebook_keymap, "C-M-o", "facebook-open-current-story-new-buffer");
-define_key(facebook_keymap, "C-O", "facebook-open-current-story-new-buffer-background");
-
 // function for inspecting and finding the link of selected story
-function facebook_mode_find_story_link(I){
+function facebook_mode_find_story_link(I, open_url_func){
   var doc = I.buffer.document;
   var selectedStory = doc.querySelector(".selectedStorySimple");
   if(selectedStory == null){
@@ -40,10 +26,22 @@ function facebook_mode_find_story_link(I){
 	  I.minibuffer.message("Cannot find timestamp link");
 	} else {
 	  var link = timestamp.parentNode;
-	  return link;
+	  open_url_func(link, i.window);
 	}
   }
 }
+
+// interactive command to open selected story
+// new buffer
+interactive("facebook-open-current-story-new-buffer", null, function (I) {
+  facebook_mode_find_story_link(I, load_url_in_new_buffer);
+  });
+// new buffer background
+interactive("facebook-open-current-story-new-buffer-background", null, function (I) {
+  facebook_mode_find_story_link(I, load_url_in_new_buffer_background);
+  });
+define_key(facebook_keymap, "C-M-o", "facebook-open-current-story-new-buffer");
+define_key(facebook_keymap, "C-O", "facebook-open-current-story-new-buffer-background");
 
 define_keymaps_page_mode("facebook-mode",
     build_url_regexp($domain = "facebook",
