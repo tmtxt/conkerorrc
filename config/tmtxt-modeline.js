a3c66deaaebf69ac696e5acfbe420756027b9c0f
tmtxt <tmtxt@txt-mac.local>
2013-09-17 00:02:05 +0700

    edit cmmb image path

--- config/tmtxt-modeline.js
+++ config/tmtxt-modeline.js
@@ -10,11 +10,20 @@ add_hook("mode_line_hook", mode_line_adder(loading_count_widget), true);
 let (path = get_home_directory()) {
   // add to load path
   path.appendRelativePath(".conkerorrc");
-  path.appendRelativePath("conkeror-mac-modeline-buttons");
+  path.appendRelativePath("modules");
+  path.appendRelativePath("cmmb");
   load_paths.unshift(make_uri(path).spec);
 
   // include the library
   require("conkeror-mac-modeline-buttons.js");  
 };
+
+// set the image path for cmmb
+cmmb_image_path = get_home_directory();
+cmmb_image_path.append(".conkerorrc");
+cmmb_image_path.append("modules");
+cmmb_image_path.append("cmmb");
+cmmb_image_path.append("images");
+cmmb_image_path = make_uri(cmmb_image_path).spec;
 
 provide("tmtxt-modeline");
