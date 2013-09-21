2907ac68f2e6ee1c7be644d36f97afd104d93796
tmtxt <tmtxt@txt-mac.local>
2013-09-21 12:38:28 +0700

    move fb share to tmtxt-facebook.js

--- config/tmtxt-facebook.js
+++ config/tmtxt-facebook.js
@@ -1,4 +1,16 @@
-// extended facebook mode
+//Facebook share
+function facebook_share(I){
+    var d=I.buffer.document;
+    var f='http://www.facebook.com/sharer';
+    var l=d.location, e=encodeURIComponent;
+    var p='.php?src=bm&v=4&i=1279479932&u='+e(l.href)+'&t='+e(d.title);
+    browser_object_follow(I.buffer,
+                          OPEN_NEW_BUFFER,
+                          f+p);
+};
+interactive("facebook-share", "Share the current site on Facebook.", facebook_share);
+
+// Conkeror Extended Facebook Mode
 let (path = get_home_directory()) {
   // add to load path
   path.appendRelativePath(".conkerorrc");
--- config/tmtxt-sites.js
+++ config/tmtxt-sites.js
@@ -1,17 +1,5 @@
 // stuff for interact with specific sites
 
-//Facebook share
-function facebook_share(I){
-    var d=I.buffer.document;
-    var f='http://www.facebook.com/sharer';
-    var l=d.location, e=encodeURIComponent;
-    var p='.php?src=bm&v=4&i=1279479932&u='+e(l.href)+'&t='+e(d.title);
-    browser_object_follow(I.buffer,
-                          OPEN_NEW_BUFFER,
-                          f+p);
-};
-interactive("facebook-share", "Share the current site on Facebook.", facebook_share);
-
 // get tiny url for the current page
 // press * q and then c to generate and copy the tinyurl into clipboard
 define_browser_object_class(
