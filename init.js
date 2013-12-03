89ee5749535301709ef9e536d88dea31f0dd9f22
tmtxt <tmtxt@Truongs-Macbook.local>
2013-12-03 14:03:02 +0700

    add sqlite manager add on

--- init.js
+++ init.js
@@ -73,11 +73,20 @@ function repl_context() {
 // Allow installing extension from any source
 session_pref("xpinstall.whitelist.required", false);
 
-//viewmarks extension, to manage bookmarks
+// viewmarks extension, to manage bookmarks
+// requirement: viewmarks extension https://addons.mozilla.org/en-US/firefox/addon/viewmarks/
 interactive("viewmarks",
     "Open ViewMarks window.",
     function (I) {
         make_chrome_window('chrome://viewmarks/content/viewmark.xul');
+    });
+
+// sqlite manager extension
+// requirements: sqlite manager https://addons.mozilla.org/en-US/firefox/addon/sqlite-manager/
+interactive("sqlite-manager",
+    "Open SQLite Manager window.",
+    function (I) {
+        make_chrome_window('chrome://SQLiteManager/content/sqlitemanager.xul');
     });
 
 /// auto-exit hinting
