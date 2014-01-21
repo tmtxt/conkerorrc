648afe811900d39dc4e432e7b8340a19360f634d
tmtxt <tmtxt@tmtxt.local>
2014-01-21 11:03:17 +0700

    function for batch defining, undefining keys/key aliases

--- config/tmtxt-keybindings.js
+++ config/tmtxt-keybindings.js
@@ -11,28 +11,103 @@ function previous_buffer(I){
   hide_tab_delay();
 }
 
-define_key(default_global_keymap, "A-z", previous_buffer);
-define_key(default_global_keymap, "C-j", previous_buffer);
-define_key(default_global_keymap, "A-x", next_buffer);
-define_key(default_global_keymap, "C-l", next_buffer);
+// define keys for specific keymap
+// arguments: keymap, keystroke1, command1, keystroke2, command2,...
+function tmtxt_define_keys(){
+  // check if arguments if larger than 1
+  if(arguments.length > 1){
+	var args = Array.prototype.slice.call(arguments);
+	
+	// get the keymap
+	var keymap = args[0];
+
+	// remove the first item (keymap)
+	args.splice(0, 1);
+
+	// the length
+	var length = 0;
+
+	// check the number of elements in arguments
+	if(args.length % 2 == 0){
+	  length = args.length;
+	} else {
+	  length = args.length + 1;
+	  // add one more null item to the array
+	  args[length] = null;
+	}
+
+	// loop through the arguments
+	for(var i = 0; i < length; i+=2){
+	  define_key(keymap, args[i], args[i+1]);
+	}
+  }
+}
+
+// define key aliases for each pair in the input arguments
+function tmtxt_define_keys_aliases(){
+  var args = Array.prototype.slice.call(arguments);
+  
+  // check if the number of arguments is even
+  var length;
+  if(args.length % 2 == 0){
+	length = args.length;
+  } else {
+	length = args.length + 1;
+	args[length] = null;
+  }
+
+  // loop through the args
+  for(var i = 0; i < length; i+=2){
+	define_key_alias(args[i], args[i+1]);
+  }
+}
+
+// undefine all input keys for the specific keymap
+// arguments: keymap, key1, key2, key3,...
+function tmtxt_undefine_keys(){
+  var args = Array.prototype.slice.call(arguments);
+
+  // check if the arguments number is bigger than 1
+  if(args.length > 1){
+	var keymap = args[0];
+
+	args.splice(0,1);
+
+	// loop through the args
+	for(var i = 0; i < args.length; i++){
+	  undefine_key(keymap, args[i]);
+	}
+  }
+}
+
+tmtxt_define_keys(default_global_keymap,
+				  "A-z",		previous_buffer,
+				  "C-j",		previous_buffer,
+				  "A-x",		next_buffer,
+				  "C-l",		next_buffer,
+				  "O",			"find-url-new-buffer",
+				  "C-x C-d",	"find-alternate-url"
+				 );
+
+tmtxt_undefine_keys(content_buffer_normal_keymap, "F", "C-f");
 
 // follow new buffer background
 define_key(content_buffer_normal_keymap, "A-f", "follow-new-buffer-background");
-undefine_key(content_buffer_normal_keymap, "F");
+// undefine_key(content_buffer_normal_keymap, "F");
 define_key(content_buffer_normal_keymap, "F", "follow-new-buffer-background");
 
 // find url new buffer
-define_key(default_global_keymap, "O", "find-url-new-buffer");
+// define_key(default_global_keymap, "O", "find-url-new-buffer");
 // undefine_key(content_buffer_normal_keymap, "t");
 // undefine_key(default_global_keymap, "t");
 // define_key(default_global_keymap, "t", "find-url-new-buffer");
-define_key(default_global_keymap, "C-x C-d", "find-alternate-url");
+// define_key(default_global_keymap, "C-x C-d", "find-alternate-url");
 
 // ergonomic layout for navigation
 // for MacOS, the key binding with M- got problem, use KeyRemap4Macbook to fix
 
 // undefine key first
-undefine_key(content_buffer_normal_keymap, "C-f");
+// undefine_key(content_buffer_normal_keymap, "C-f");
 undefine_key(text_keymap, "C-f");
 undefine_key(content_buffer_normal_keymap, "C-b");
 undefine_key(text_keymap, "C-b");
@@ -82,9 +157,14 @@ define_key(default_global_keymap, "A-q" , "quit");
 define_key(content_buffer_normal_keymap, "C-A-v", "paste-url-new-buffer");
 
 //// Key Aliases
-define_key_alias("C-m", "return");
-define_key_alias("A-c", "M-w");
-define_key_alias("A-v", "C-y");
-define_key_alias("C-o", "escape");
+// define_key_alias("C-m", "return");
+// define_key_alias("A-c", "M-w");
+// define_key_alias("A-v", "C-y");
+// define_key_alias("C-o", "escape");
+
+tmtxt_define_keys_aliases("C-o",			"escape",
+						"A-v",			"C-y",
+						"A-c",			"M-w",
+						"C-m",			"return");
 
 provide("tmtxt-keybindings");
