ff8cbc2a95143220fbf5b7b646a1374347f293fa
tmtxt <tmtxt@Trans-MacBook-Pro.local>
2014-03-05 17:12:03 +0700

    integrate password manager to mac keychain

--- config/tmtxt-form.js
+++ config/tmtxt-form.js
@@ -4,6 +4,9 @@
 user_pref("signon.prefillForms", true);
 user_pref("signon.autofillForms", true);
 user_pref("signon.rememberSignons", true);
+user_pref("signon.expireMasterPassword", false);
+user_pref("signon.SignonFileName", "signons.txt");
+Cc["@mozilla.org/login-manager;1"].getService(Ci.nsILoginManager); // init
 
 // Enable Password Manager
 Components.classes["@mozilla.org/login-manager;1"]
