528d2c14955f09c9eba4cb4ae27407692ec96887
tmtxt <tmtxt@tmtxt-mac.local>
2014-04-21 21:09:59 +0700

    fix focus color when toggle dark color

--- config/tmtxt-appearance.js
+++ config/tmtxt-appearance.js
@@ -50,8 +50,9 @@ interactive("colors-toggle", "toggle between document and forced colors",
                 session_pref("browser.anchor_color", "#4986dd");
                 session_pref("browser.display.background_color", "#0C141E");
                 session_pref("browser.display.foreground_color", "#A0AFA8");
-                session_pref("browser.display.focus_background_color", "green"); // ?
-                session_pref("browser.display.focus_text_color", "red"); // ?
+                session_pref("browser.display.use_focus_colors", true);
+                session_pref("browser.display.focus_background_color", "#062B35"); // ?
+                session_pref("browser.display.focus_text_color", "#30A098"); // ?
                 session_pref("browser.visited_color", "#805DBB");
                 session_pref(p, false);
               } else {
@@ -59,6 +60,7 @@ interactive("colors-toggle", "toggle between document and forced colors",
                 session_pref("browser.anchor_color", "#0000EE");
                 session_pref("browser.display.background_color", "#FFFFFF");
                 session_pref("browser.display.foreground_color", "#000000");
+                session_pref("browser.display.use_focus_colors", false);
                 session_pref("browser.display.focus_background_color", "#117722");
                 session_pref("browser.display.focus_text_color", "#FFFFFF");
                 session_pref("browser.visited_color", "#551A8B");
