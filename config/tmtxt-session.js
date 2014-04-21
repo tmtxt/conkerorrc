def64fb050842f4174da9bedb3cdecef1def0883
tmtxt <tmtxt@tmtxt-mac.local>
2014-04-21 21:11:41 +0700

    only commit session 10 times less

--- config/tmtxt-session.js
+++ config/tmtxt-session.js
@@ -7,8 +7,14 @@ session_auto_save_auto_load = true;
 
 // backup session everytime create/delete a new buffer
 // require the conkeror-session-backup.sh script
+var unsavedBufferNumber = 0;
 add_hook("create_buffer_hook", function(buffer){
-  shell_command_blind("sh ~/bin/conkeror-session-backup.sh");
+  unsavedBufferNumber = unsavedBufferNumber + 1;
+  if(unsavedBufferNumber === 10){
+    shell_command_blind("sh ~/bin/conkeror-session-backup.sh");
+    unsavedBufferNumber = 0;
+  }
+  
 });
 
 provide("tmtxt-session");
