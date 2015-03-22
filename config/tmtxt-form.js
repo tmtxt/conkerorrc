5b7c2c9ec64e9b7fb42582f6c415546e425c25ec
Truong <me@truongtx.me>
2015-03-22 19:12:09 +0700

    disable password manager

--- config/tmtxt-form.js
+++ config/tmtxt-form.js
@@ -6,10 +6,10 @@ user_pref("signon.autofillForms", true);
 user_pref("signon.rememberSignons", true);
 user_pref("signon.expireMasterPassword", false);
 user_pref("signon.SignonFileName", "signons.txt");
-Cc["@mozilla.org/login-manager;1"].getService(Ci.nsILoginManager); // init
+// Cc["@mozilla.org/login-manager;1"].getService(Ci.nsILoginManager); // init
 
 // Enable Password Manager
-Components.classes["@mozilla.org/login-manager;1"]
-    .getService(Components.interfaces.nsILoginManager);
+// Components.classes["@mozilla.org/login-manager;1"]
+//     .getService(Components.interfaces.nsILoginManager);
 
 provide("tmtxt-form");
