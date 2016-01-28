ac3227ca29c9e079b60d8d30c4101f131bbe89f5
Tran Xuan Truong <truong.tran@lazada.com>
2016-01-28 13:28:33 +0700

    refactor development

--- config/tmtxt-development.js
+++ config/tmtxt-development.js
@@ -11,15 +11,15 @@ let (mozrepl_init = get_home_directory()) {
 ////////////////////////////////////////////////////////////////////////////////
 // firebug
 define_variable("firebug_url",
-    "http://getfirebug.com/releases/lite/1.2/firebug-lite-compressed.js");
+                "http://getfirebug.com/releases/lite/1.2/firebug-lite-compressed.js");
 
 function firebug (I) {
-    var doc = I.buffer.document;
-    var script = doc.createElement('script');
-    script.setAttribute('type', 'text/javascript');
-    script.setAttribute('src', firebug_url);
-    script.setAttribute('onload', 'firebug.init();');
-    doc.body.appendChild(script);
+  var doc = I.buffer.document;
+  var script = doc.createElement('script');
+  script.setAttribute('type', 'text/javascript');
+  script.setAttribute('src', firebug_url);
+  script.setAttribute('onload', 'firebug.init();');
+  doc.body.appendChild(script);
 }
 interactive("firebug", "open firebug lite", firebug);
 
