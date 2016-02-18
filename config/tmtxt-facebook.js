07ead671fd384bfacf1d0bd1059b8d36e75fc556
Tran Xuan Truong <truong.tran@lazada.com>
2016-02-18 13:56:32 +0700

    add notification permission for messenger

--- config/tmtxt-facebook.js
+++ config/tmtxt-facebook.js
@@ -1,3 +1,5 @@
+var tmtxt = tmtxt || {};
+
 ////////////////////////////////////////////////////////////////////////////////
 // Conkeror Extended Facebook Mode
 (function(){
@@ -12,5 +14,21 @@
   require("conkeror-extended-facebook-mode.js");
 }());
 interactive("facebook-share", "Share the current site on Facebook.", cefm.facebookShare);
+
+// Facebook messeger (messenger.com)
+(function(){
+  // init permission manager
+  tmtxt.xpcom = tmtxt.xpcom || {};
+  const permissionManager = Components.classes["@mozilla.org/permissionmanager;1"]
+        .getService(Components.interfaces.nsIPermissionManager);
+  tmtxt.xpcom.permissionManager = permissionManager;
+
+  // add permission for messeger.com
+  permissionManager.add(
+    make_uri("http://www.messenger.com/"),
+    "desktop-notification",
+    Components.interfaces.nsIPermissionManager.ALLOW_ACTION
+  );
+}());
 
 provide("tmtxt-facebook");
